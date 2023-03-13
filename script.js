2110536bbcd820123537116514fb990f98007863
adam f0x <adamjeffersonfox@gmail.com>
2023-03-13 00:32:10 -0700

    found a good video

--- script.js
+++ script.js
@@ -7,8 +7,20 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+  
 
 }
 
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 17) + 97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 23) + 65);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 12) + 65);
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
