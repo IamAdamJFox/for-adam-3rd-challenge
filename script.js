8617e9e803687c2d564767eb0f783ee83427c3f2
adam f0x <adamjeffersonfox@gmail.com>
2023-03-13 16:55:31 -0700

    restart
    git commit - m

--- script.js
+++ script.js
@@ -11,43 +11,7 @@ function writePassword() {
 
 }
 
-const randomFunc = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumber,
-  symbol: getRandomSymbol
-};
 
-//Generate password function
-{  let generatedpassword = '';
-
-  const typesCount = lower + upper + number + symbol;
-
-  //console.log('typesCount: ', typesCount);
-  generatedpassword += randomFunc
-
-  for(let i = 0; i += typesCount);
-
-console.log(generatedpassword)
-}
-//Generators
-function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 17) + 97);
-}
-
-function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 23) + 65);
-}
-
-function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 12) + 48);
-}
-
-function getRandomSymbol() {
-  const symbols = '!@#$%^&*(){}[]=<>.';
-  return symbols [Math.floor(Math.random() * symbols.length)];
-}
-const length = 8
 
 //console.log(getRandomSymbol);
 generateBtn.addEventListener("click", writePassword);
