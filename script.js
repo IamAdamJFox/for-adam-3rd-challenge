81016c4c307c90b99eb79357406f7670d86504bc
adam f0x <adamjeffersonfox@gmail.com>
2023-03-13 18:30:50 -0700

    ask bcs

--- script.js
+++ script.js
@@ -2,22 +2,31 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
 
+}
+
+//Constants
 const length = 8;
 const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijkmnopqrstuwyx0123456789!@#$%^&*()[]{}=+<>";
-
+//Generator function
 function generatePassword () {
   let pass = "";
-  let chars = "";
   for (var i = 0; i <= length; i++) {
     let rand = Math.floor(Math.random() * chars.length
     );
   
   pass += chars.substring(rand, rand +1)
+  }
+  console.log(pass);
 
+  
 }
 
-}
 
 
-generateBtn.addEventListener("click", generatePassword);
+
+generateBtn.addEventListener("click", writePassword);
